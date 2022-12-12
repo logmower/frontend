a9d1211d83cd7c47efb9df4afa8f882a2c80a9c3
Erki Aas <eaas@k-space.ee>
2022-12-12 23:17:55 +0200

    Add VueViewer image viewer

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import Toaster from "@meforma/vue-toaster";
 import VCalendar from 'v-calendar';
+import VueViewer from 'v-viewer'
 import { loadFonts } from './plugins/webfontloader'
 import './assets/main.css'
 import 'vue-select/dist/vue-select.css';
 import 'v-calendar/dist/style.css';
+import 'viewerjs/dist/viewer.css'
 loadFonts()
 
 const app = createApp(App);
@@ -15,5 +17,10 @@ app.use(store);
 app.use(vuetify);
 app.use(Toaster);
 app.use(VCalendar, {});
+app.use(VueViewer, {
+    defaultOptions: {
+        zIndex: 9999
+    }
+})
 app.mount("#app");
 
